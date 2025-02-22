a2cefe8c02577ac0d665f9c29b0e7665acb3a226
Sanam Yadav <jstsanam@gmail.com>
2025-02-22 07:56:44 +0530

    Added socket.io for handling notifications in frontend

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-import { app } from "./src/app.js";
 import { connectDB } from "./src/config/db.js";
 import { ENV } from "./src/config/environment.js";
+import server from "./src/server.js";
 
+// Connect to the database
 connectDB();
 
-app.listen(ENV.PORT, () => {
+server.listen(ENV.PORT, () => {
   console.log(`Server running on ${ENV.PORT}`);
 });
